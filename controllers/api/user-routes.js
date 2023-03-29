dd87614e2112d7418cfc07bf73faf95a26beb879
Ruben Franco <ruben.afranco@yahoo.com>
2023-03-28 21:04:22 -0700

    added getAll users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,3 +4,17 @@ const {
     Post,
     Comment
 } = require('../../models');
+
+// Get all users
+router.get('/', (req, res) => {
+    User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
