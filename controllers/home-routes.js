0b45d7a7e3065c0825643b70644cca31a1fd4f69
Ruben Franco <ruben.afranco@yahoo.com>
2023-03-28 20:09:14 -0700

    created login and sign up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -92,3 +92,21 @@ router.get('/post/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
